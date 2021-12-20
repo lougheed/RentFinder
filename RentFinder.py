9d62209f10b8ad58898aafbd087c2cc2f80a090e
karl <dragonkarl45@gmail.com>
2021-12-20 20:27:28 +0000

    Update RentFinder.py

--- RentFinder.py
+++ RentFinder.py
@@ -4,11 +4,14 @@ from bs4 import BeautifulSoup
 r = requests.get('https://www.daft.ie/property-for-rent/ireland/houses?location=cork-commuter-towns-cork&location=cork-city').text
 soup = BeautifulSoup(r, 'html.parser')
 data = soup.find_all("div", class_="Card__Body-x1sjdn-3 dhiEPC")
-links = soup.find('a')
-
+#links = soup.find_all("a",href=True,class_="SearchPage__Result-gg133s-2 itNYNv")
+MyList = []
 for datas in data:
-    price = datas.find("div", class_="TitleBlock__Price-sc-1avkvav-3 pJtsY").text
-    address = datas.find("p", class_="TitleBlock__Address-sc-1avkvav-7 knPImU").text
-    URL = datas.find_all('a')
-    print(f"Address:{address} Price is {price}")
-    print("test link:", URL)
+    price = datas.find("div", class_="TitleBlock__Price-sc-1avkvav-3 pJtsY").text# get the raw price in text
+    address = datas.find("p", class_="TitleBlock__Address-sc-1avkvav-7 knPImU").text#get the adresss text
+    both = address + " " + price
+    MyList.append(both)
+
+    #print(f"Address:{address} Price is {price}")
+
+print(MyList)
