c8f82fe714c1c4c43e19f552f91a995d48a1400d
karl <dragonkarl45@gmail.com>
2021-12-29 15:09:45 +0000

    Update RentFinder.py
    Added a way to send emails with the data set. Next step is the clean the data output up a bit and make it look nicer.

--- RentFinder.py
+++ RentFinder.py
@@ -1,5 +1,31 @@
 import requests
+import smtplib, ssl
 from bs4 import BeautifulSoup
+import sys
+sys.path.insert(0, '/Users/Root/Desktop/Python')
+from GmailConfig import username, password#all 3 lines used to import password hosted in another file/folger for secure handleing
+from datetime import date
+from email.mime.text import MIMEText
+
+def SendMail(DataOutput):
+    port = 465  # For SSL
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    sender_email = "pythonbox65@gmail.com"
+    receiver_email = "dragonkarl45@gmail.com"
+    TheTime =  date.today()#pulls in the date
+    #set up the msg to send
+    msg = MIMEText(f'Palces to Rent report for {TheTime}')
+    msg['Subject'] = f"""Rents in Cork for {TheTime}
+    Here is the data:\n {DataOutput}
+    """
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(
+            sender_email, receiver_email,msg.as_string())
 
 r = requests.get('https://www.daft.ie/property-for-rent/ireland/houses?location=cork-commuter-towns-cork&location=cork-city').text
 soup = BeautifulSoup(r, 'html.parser')
@@ -9,9 +35,11 @@ MyList = []
 for datas in data:
     price = datas.find("div", class_="TitleBlock__Price-sc-1avkvav-3 pJtsY").text# get the raw price in text
     address = datas.find("p", class_="TitleBlock__Address-sc-1avkvav-7 knPImU").text#get the adresss text
-    both = address + " " + price
+    both = address + "\n" + price
     MyList.append(both)
 
     #print(f"Address:{address} Price is {price}")
-
-print(MyList)
+separator = ", "# useed for the join() method in python
+DataOutput = separator.join(MyList)#Takes the list and makes it look nicer, will use this as a payloud in the email.
+SendMail(DataOutput)
+print("Done")
